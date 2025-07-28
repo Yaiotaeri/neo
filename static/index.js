d7f79a25ff7b32d01254aefb3fef5ec0ddf10f80
Yaiotaeri <testevscode5566@gmail.com>
2025-07-28 06:13:53 -0300

    novos estilos

--- static/index.js
+++ static/index.js
@@ -8,14 +8,19 @@ btnMenu.addEventListener('click', ()=>{
 });
 
 
-const goWtszp = document.getElementById('goWtszp');
-
-goWtszp.addEventListener('click',()=>{
-    window.open('https://api.whatsapp.com/send?phone=5535988874386&text=Ol%C3%A1%20vim%20pelo%20seu%20site%20%F0%9F%98%81', '_blank');
-});
-
 const btnWtszp = document.getElementById('btnWtszp');
 
 btnWtszp.addEventListener('click',()=>{
     window.open('https://api.whatsapp.com/send?phone=5535988874386&text=Ol%C3%A1%20vim%20pelo%20seu%20site%20%F0%9F%98%81', '_blank');
+});
+
+const btnWt = document.querySelectorAll('.btnWt');
+
+btnWt.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        window.open(
+            'https://api.whatsapp.com/send?phone=5535988874386&text=Ol%C3%A1%20vim%20pelo%20seu%20site%20%F0%9F%98%81',
+            '_blank'
+        );
+    });
 });
